be7b21a9a95a7a0de89b641392d966c9755d2ec3
Leon Anneken <83974950+leontm-dev@users.noreply.github.com>
2024-03-08 13:50:20 +0100

    Create Modes.cs

---
+++ Modes.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaschenrechnerLib
+{
+    public enum Modes
+    {
+        Plus = 0,
+        Minus = 1,
+        Multiply = 2,
+        Divide = 3,
+        Modulo = 4,
+        None = 5,
+    }
+}
