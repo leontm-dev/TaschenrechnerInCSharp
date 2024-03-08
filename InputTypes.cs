90a9d1b89e485c4ae7123fc6b747a27edbbd02bb
Leon Anneken <83974950+leontm-dev@users.noreply.github.com>
2024-03-08 13:50:10 +0100

    Create InputTypes.cs

---
+++ InputTypes.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaschenrechnerLib
+{
+    public enum InputTypes
+    {
+        Mode = 0,
+        Calculation = 1,
+        Cancel = 2,
+    }
+}
